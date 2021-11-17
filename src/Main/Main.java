1b40d4c5de618510655732569d8bc21333243235
Jeremy Ramirez <ujeremyr@gmail.com>
2021-11-17 17:24:37 -0600

    Setters, getters and main class

---
+++ src/Main/Main.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Panel aplicacion = new Panel();
+        //aplicacion.setVisible(true);
+    }
+}
---
+++ src/set_get/set_get.java
@@ -0,0 +1,38 @@
+package set_get;
+
+public class set_get {
+
+    /**Variables a recuperar**/
+
+    private String puntos_salida;
+    private String puntos_destino;
+    private String info_lugares;
+
+
+    /**Metodos de get and set de las variables anteriores**/
+    public String getPuntos_salida() {
+        return puntos_salida;
+    }
+
+    public void setPuntos_salida(String puntos_salida) {
+        this.puntos_salida = puntos_salida;
+    }
+
+    public String getPuntos_destino() {
+        return puntos_destino;
+    }
+
+    public void setPuntos_destino(String puntos_destino) {
+        this.puntos_destino = puntos_destino;
+    }
+
+    public String getInfo_lugares() {
+        return info_lugares;
+    }
+
+    public void setInfo_lugares(String info_lugares) {
+        this.info_lugares = info_lugares;
+    }
+
+
+}
